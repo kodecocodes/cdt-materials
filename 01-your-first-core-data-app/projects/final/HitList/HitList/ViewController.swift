34a3b39bee8718f0c4fd489c7725aaa03678de4c
Darren Ferguson <darren102@gmail.com>
2020-09-06 15:10:04 +0100

    Tech Edit Chapter 1 complete

--- 01-your-first-core-data-app/projects/final/HitList/HitList/ViewController.swift
+++ 01-your-first-core-data-app/projects/final/HitList/HitList/ViewController.swift
@@ -35,90 +35,92 @@ import CoreData
 
 class ViewController: UIViewController {
 
-    var people: [NSManagedObject] = []
 
-    @IBOutlet weak var tableView: UITableView!
+  @IBOutlet weak var tableView: UITableView!
+  var people: [NSManagedObject] = []
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
+  override func viewDidLoad() {
+    super.viewDidLoad()
 
-        title = "The List"
-        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "Cell")
+    title = "The List"
+    tableView.register(UITableViewCell.self, forCellReuseIdentifier: "Cell")
+  }
+
+  override func viewWillAppear(_ animated: Bool) {
+    super.viewWillAppear(animated)
+
+    guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else {
+      return
     }
 
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
+    let managedContext = appDelegate.persistentContainer.viewContext
+    let fetchRequest = NSFetchRequest<NSManagedObject>(entityName: "Person")
 
-        guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else {
-            return
-        }
+    do {
+      people = try managedContext.fetch(fetchRequest)
+    } catch let error as NSError {
+      print("Could not fetch. \(error), \(error.userInfo)")
+    }
+  }
 
-        let managedContext = appDelegate.persistentContainer.viewContext
-        let fetchRequest = NSFetchRequest<NSManagedObject>(entityName: "Person")
+  @IBAction func addName(_ sender: UIBarButtonItem) {
+    let alert = UIAlertController(title: "New Name",
+                                  message: "Add a new name",
+                                  preferredStyle: .alert)
 
-        do {
-            people = try managedContext.fetch(fetchRequest)
-        } catch let error as NSError {
-            print("Could not fetch. \(error), \(error.userInfo)")
-        }
+    let saveAction = UIAlertAction(title: "Save", style: .default) { [unowned self] action in
+      guard let textField = alert.textFields?.first,
+            let nameToSave = textField.text else {
+        return
+      }
+
+      self.save(name: nameToSave)
+      self.tableView.reloadData()
     }
 
-    @IBAction func addName(_ sender: UIBarButtonItem) {
-        let alert = UIAlertController(title: "New Name", message: "Add a new name", preferredStyle: .alert)
+    let cancelAction = UIAlertAction(title: "Cancel", style: .cancel)
 
-        let saveAction = UIAlertAction(title: "Save", style: .default) { [unowned self] action in
-            guard let textField = alert.textFields?.first,
-                  let nameToSave = textField.text else {
-                return
-            }
+    alert.addTextField()
 
-            self.save(name: nameToSave)
-            self.tableView.reloadData()
-        }
+    alert.addAction(saveAction)
+    alert.addAction(cancelAction)
 
-        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel)
+    present(alert, animated: true)
+  }
 
-        alert.addTextField()
+  func save(name: String) {
 
-        alert.addAction(saveAction)
-        alert.addAction(cancelAction)
-
-        present(alert, animated: true)
+    guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else {
+      return
     }
 
-    func save(name: String) {
+    let managedContext = appDelegate.persistentContainer.viewContext
 
-        guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else {
-            return
-        }
+    let entity = NSEntityDescription.entity(forEntityName: "Person", in: managedContext)!
+    let person = NSManagedObject(entity: entity, insertInto: managedContext)
+    person.setValue(name, forKeyPath: "name")
 
-        let managedContext = appDelegate.persistentContainer.viewContext
-
-        let entity = NSEntityDescription.entity(forEntityName: "Person", in: managedContext)!
-        let person = NSManagedObject(entity: entity, insertInto: managedContext)
-        person.setValue(name, forKeyPath: "name")
-
-        do {
-            try managedContext.save()
-            people.append(person)
-        } catch let error as NSError {
-            print("Could not save. \(error), \(error.userInfo)")
-        }
+    do {
+      try managedContext.save()
+      people.append(person)
+    } catch let error as NSError {
+      print("Could not save. \(error), \(error.userInfo)")
     }
+  }
 }
 
 // MARK: - UITableViewDataSource
 extension ViewController: UITableViewDataSource {
 
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return people.count
-    }
+  func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    people.count
+  }
 
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+  func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
 
-        let person = people[indexPath.row]
-        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
-        cell.textLabel?.text = person.value(forKeyPath: "name") as? String
-        return cell
-    }
+    let person = people[indexPath.row]
+    let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+    cell.textLabel?.text = person.value(forKeyPath: "name") as? String
+    return cell
+  }
 }
