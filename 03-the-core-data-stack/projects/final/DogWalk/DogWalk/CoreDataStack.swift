726e68490738ba4d8b14e52f273071203ca30c16
Pietro Rea <pietro@sweetpeamobile.com>
2020-09-08 20:31:58 -0400

    Removing random white space

--- 03-the-core-data-stack/projects/final/DogWalk/DogWalk/CoreDataStack.swift
+++ 03-the-core-data-stack/projects/final/DogWalk/DogWalk/CoreDataStack.swift
@@ -34,19 +34,19 @@ import Foundation
 import CoreData
 
 class CoreDataStack {
-  
+
   private let modelName: String
-  
+
   lazy var managedContext: NSManagedObjectContext = {
     return self.storeContainer.viewContext
   }()
-  
+
   init(modelName: String) {
     self.modelName = modelName
   }
-  
+
   private lazy var storeContainer: NSPersistentContainer = {
-    
+
     let container = NSPersistentContainer(name: self.modelName)
     container.loadPersistentStores {
       (storeDescription, error) in
@@ -56,10 +56,10 @@ class CoreDataStack {
     }
     return container
   }()
-  
+
   func saveContext () {
     guard managedContext.hasChanges else { return }
-    
+
     do {
       try managedContext.save()
     } catch let error as NSError {
